0a2db2ba753ec25fef56049a651bb27954b5e8a5
石田健@再春館製薬所 <t.ishida@saishunkan.co.jp>
2025-06-25 15:17:02 +0900

    fix: FFmpegのロードエラーに対応するためViteの設定を更新

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ↓ serverプロパティとheadersの設定が追加されていることを確認
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
